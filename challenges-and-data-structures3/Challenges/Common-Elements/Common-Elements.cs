47abae97accffb620f86fbed09eacabf52d55701
RawanYaghmour <Rawan.yag02@gmail.com>
2024-07-01 20:42:13 +0300

    add cc6

---
+++ challenges-and-data-structures3/Challenges/Common-Elements/Common-Elements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace challenges_and_data_structures3.Challenges.Common_Elements
+{
+    public class Common_Elements
+    {
+        public static int[] CommonElements(int[] arr1, int[] arr2)
+        {
+            List<int> commonElements = new List<int>();
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (arr1[i] == arr2[j] && !commonElements.Contains(arr1[i]))
+                    {
+                        commonElements.Add(arr1[i]);
+                    }
+                }
+            }
+            return commonElements.ToArray();
+        }
+
+    }
+
+}
--- challenges-and-data-structures3/Program.cs
+++ challenges-and-data-structures3/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace challenges_and_data_structures3
+﻿using challenges_and_data_structures3.Challenges.Common_Elements;
+
+namespace challenges_and_data_structures3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array1 = { 1, 2, 3, 0 };
+            int[] array2 = { 2, 3, 4, 9 };
+
+            int[] result = Common_Elements.CommonElements(array1, array2);
+            Console.WriteLine("Common Elements: " + string.Join(", ", result));
         }
     }
 }
